bc34a10457712fa152b17b503863b24696c26d59
rls347 <rls347@gmail.com>
2023-11-14 17:26:47 -0800

    Changed movie codes to read in a list of files rather than a directory

--- queryplots.py
+++ queryplots.py
@@ -49,17 +49,25 @@ def sanityprint(modeldir):
     return
 
 
-def precipmovie(modeldir,name):
-    files = sorted(glob.glob(modeldir+'/*h5'))
+def precipmovie(files,name):
     xs,ys,height = get_dims(files[0])
     pcp = []
+    pcpvar = []
     for a,fil in enumerate(files):
         rain = rainrate(fil)
         if np.max(rain)>np.min(rain):
             pcp.append(rain)
+            pcpvar.append(np.mean(rain))
     pcp = np.array(pcp)
 
     if len(pcp)>0:
+        time = np.arange(len(pcpvar))
+        plt.clf()
+        plt.plot(time,pcpvar)
+        plt.title('Domain Mean Rain Rate')
+        plt.savefig(name+'pcpratetimeseries.png')
+        plt.close()
+
         fig = plt.figure()
         levels=np.linspace(0.1,np.max(rain),20)
         fargs = [pcp,xs,ys,levels,"Precip Rate (mm/hr)"]
@@ -71,16 +79,16 @@ def precipmovie(modeldir,name):
     return
 
 
-def maxslicemovie(modeldir,name,var='cond'):
-    files = sorted(glob.glob(modeldir+'/*h5'))
+def maxslicemovie(files,name,var='cond'):
     print(len(files))
     xs,ys,height = get_dims(files[0])
     var3d = []
     for f in files:
+        print(f)
         if var == 'cond':
             q=sum_cond(f)
         elif var == 'w':
-            q = getvar(f,'WC')
+            q = getvar(f,'WP')
         else:
             q = getvar(f,var)
         if np.max(q)>np.min(q):
@@ -98,12 +106,11 @@ def maxslicemovie(modeldir,name,var='cond'):
     return
 
 
-def sfctempmovie(modeldir,name):
-    files = sorted(glob.glob(modeldir+'/*h5'))
+def sfctempmovie(files,name):
     xs,ys,height = get_dims(files[0])
     temp3d = []
     for a,fil in enumerate(files):
-        sfctemp = get_tempk(fil)[1,:,:] #first point above ground
+        sfctemp = get_tempk(fil,'sfc') #first point above ground
         if np.max(sfctemp)>np.min(sfctemp):
             temp3d.append(sfctemp)
     temp3d= np.asarray(temp3d)
@@ -119,13 +126,12 @@ def sfctempmovie(modeldir,name):
 
     return
 
-def maxoverheadmovie(modeldir,name,var='w'):
-    files = sorted(glob.glob(modeldir+'/*h5'))
+def maxoverheadmovie(files,name,var='w'):
     xs,ys,height = get_dims(files[0])
     var3d= []
     for a,fil in enumerate(files):
         if var == 'w':
-            maxvar = np.max(getvar(fil,'WC'),0)
+            maxvar = np.max(getvar(fil,'WP'),0)
             if maxvar.max()>maxvar.min():
                 var3d.append(maxvar)
     var3d= np.asarray(var3d)
@@ -136,6 +142,35 @@ def maxoverheadmovie(modeldir,name,var='w'):
         fargs = [var3d,xs,ys,levels,'Max '+var]
         anim = animation.FuncAnimation(fig, animateoverhead, frames=var3d.shape[0],fargs=fargs)
         makemovie(anim,name+'maxwoverhead')
+    else:
+        print(name, ' has no variation in ',var)
+
+    return
+
+def pwmovie(files,name):
+    xs,ys,height = get_dims(files[0])
+    var3d = []
+    pwvar = []
+    for a, fil in enumerate(files):
+        var = intvapor(fil)
+        if var.max()>var.min():
+            pwvar.append(np.mean(var))
+            var3d.append(var)
+    var3d = np.asarray(var3d)
+
+    if len(var3d)>0:
+        time = np.arange(len(pwvar))
+        plt.clf()
+        plt.plot(time,pwvar)
+        plt.title('Domain Mean Precipitable Water')
+        plt.savefig(name+'PWtimeseries.png')
+        plt.close()
+
+        levels = np.linspace(np.min(var3d),np.max(var3d),20)
+        fig = plt.figure()
+        fargs = [var3d,xs,ys,levels,'Precipitable Water']
+        anim = animation.FuncAnimation(fig, animateoverhead, frames=var3d.shape[0],fargs=fargs)
+        makemovie(anim,name+'PW')
     else:
         print(name, ' has no variation in ',var)
 
